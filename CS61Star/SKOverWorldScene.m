999b95d3b298f6d77e45c5696b0b2c1e8fad8700
jtcramer <jason.t.cramer@gmail.com>
2012-11-12 23:32:37 -0800

    Started writing the OverWorld scene, which will manage the overworld gameplay. Some instance variables include the current map (with a method to change the currently shown map), a list of NPCs, and HUD elements shown on the screen. Concerning how maps are changed, probably the best way about going about it is embedding in the TMX metadata if an object/tile is a path to another map, and what map it goes to. The scene can handle it accordingly.

---
+++ CS61Star/SKOverWorldScene.m
@@ -0,0 +1,42 @@
+//
+//  SKOverWorldScene.m
+//  CS61Star
+//
+//  Created by Jason Cramer on 11/12/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SKOverWorldScene.h"
+
+
+@implementation SKOverWorldScene
+
+@synthesize currentMap;
+@synthesize npcList;
+@synthesize hudElements;
+
+-(id)init
+{
+    self = [super init];
+    if(self)
+    {
+        // set up stuff
+    }
+    
+    return self;
+}
+
+-(void)draw
+{
+    // Render this scene
+}
+
+-(id)changeMap:(CCTMXTiledMap *)newMap
+{
+    [self removeChild:currentMap cleanup:YES];
+    CCTMXTiledMap* oldMap = [currentMap autorelease];
+    currentMap = [newMap retain];
+    return oldMap;
+}
+
+@end
